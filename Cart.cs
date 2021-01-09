84760bbf7911310e9d4b85576c8dce5b26cb2a97
SamantaRuhee <46893573+SamantaRuhee@users.noreply.github.com>
2021-01-09 19:33:22 +0600

    files and folder structure created and controller and model created.

---
+++ Cart.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KidsShop
+{
+    public class Cart
+    {
+        public void addItem()
+        {
+
+        }
+
+        public void removeItem()
+        {
+
+        }
+
+        public void updateItem()
+        {
+
+        }
+
+
+        public void getList()
+        {
+
+        }
+    }
+}
